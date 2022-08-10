99cb174cb84a542392bb34af7193b745f1ca39fb
Kacper Kingsford <kacper.kingsford@dynatrace.com>
2022-08-10 20:28:31 +0200

    test CI7

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
+  api: 'http://localhost:8080/api',
   production: true
 };
+
+export const paths = {
+  getAllHelpOffers: '/help-offer',
+  getAllHelpOffersByExecutionUser: '/help-offer/execution-user',
+  getAllHelpOffersReservedByExecutionUser: '/help-offer/reserved-by-user',
+  countAllHelpOffers: '/help-offer/count',
+  countAllHelpOffersByExecutionUser: '/help-offer/count/execution-user',
+  countAllHelpOffersReservedByExecutionUser: '/help-offer/count/reserved-by-user',
+  signUp: '/auth/signup',
+  login: '/auth/signin',
+  createHelpOffer: '/help-offer',
+  reserveHelpOffer: '/help-offer/reserve'
+}
